296fa6dd65a172191f82b05564d2fd64f6cdc426
venn177 <venn177@gmail.com>
2015-10-24 11:23:20 -0400

    first release

--- jontron.py
+++ jontron.py
@@ -3,38 +3,35 @@ import dataset
 import re
 import time
 import random
+from random import randint
 
 REDDIT_USERNAME = '' # Bot's username
 REDDIT_PASS = '' # Bot's password
 MIN_WAIT = 60 # This is the least amount of time between bot replies. A minute, currently.
 MAX_WAIT = 1800 # This is the most amount of time between bot replies. 30 minutes, currently.
 
-SUBREDDIT_NAME = '' # Subreddit the bot is operating on 
+SUBREDDIT_NAME = 'vennbot' # Subreddit the bot is operating on 
 
 def main():
     print "Logging in..."
     r = praw.Reddit(user_agent = 'Jontron Shitposter v0.1')
     r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning = True)
-	print "Logged in."
 
     while True:
 		subreddit = r.get_subreddit(SUBREDDIT_NAME)
 		comments = subreddit.get_comments(limit=1)
 		
-		random_reply = random.choice(('Sorry, I only speak to sailors.', 'Get out of our home!', 'Actually, the crowbar snapped in two.', 'Quality post!', 'I have a theory! I think ' + person1 + ' is actually ' + person2 + '!'))
 		person1 = random.choice(('JonTron', 'Egoraptor', 'Arin', 'Danny', 'Jacques', 'Starcade Man'))
 		person2 = random.choice(('JonTron', 'Egoraptor', 'Arin', 'Danny', 'Jacques', 'Starcade Man', 'dead'))
-		
-        for comment in comments:
-            if comment.id not in already_done:
-				comment.reply(random_reply)
-				print: "Replying..."
-				already_done.add(comment.id)
+		random_reply = random.choice(('Sorry, I only speak to sailors.', 'Get out of our home!', 'Actually, the crowbar snapped in two.', 'Quality post!', 'I have a theory! I think ' + person1 + ' is actually ' + person2 + '!'))
 
-        # Sleep time is how I'm determining what comment it replies to. There is probably a much better way to do this.
+		for comment in comments:
+			comment.reply(random_reply)
+			print "Replying..."
+		# Sleep time is how I'm determining what comment it replies to. There is probably a much better way to do this.
 		sleepTime = randint(MIN_WAIT,MAX_WAIT)
-		print "Sleeping for " + sleepTime + " seconds..."
-        time.sleep(sleepTime) # Sleep time is in seconds.
+		print "Sleeping for " + str(sleepTime) + " seconds..."
+		time.sleep(sleepTime) # Sleep time is in seconds.
 
 if __name__ == "__main__":
     main()
